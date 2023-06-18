66c5e86a2b339e90e70d00853510560afdd76f68
Rozalyn <96503223+ZolaraCodes@users.noreply.github.com>
2023-06-18 15:19:51 -0400

    added storybook

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AirQualityCalculator from "./calculator";
+
+const App = () => {
+  return (
+    <div>
+      <h1>CADR Conscious</h1>
+      <AirQualityCalculator />
+    </div>
+  );
+};
+
+export default App;
